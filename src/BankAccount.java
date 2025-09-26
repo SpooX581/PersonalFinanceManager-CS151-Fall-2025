49f929956d7b935a47f045fe2b60bbf093a38362
lamchy3157 <chynalam@outlook.com>
2025-09-26 14:26:31 -0700

    Second commit: update BankAccount

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -26,6 +26,36 @@ public class BankAccount {
         return accountType;
     }
 
+    public void withdraw(double amount){ 
+        if(amount <= balance){ 
+            balance = balance- amount;
+            System.out.println("Accepted");
+        }
+        else{
+            System.out.println("Declined");
+        }
+    }
+
+    public void deposit(double amount){
+        if(amount <= 0){
+            System.out.println("Amount deposited has to be positive");
+        }
+        else{
+            balance = balance + amount;
+            System.out.println("Ammount accepted");
+        }
+    }
+
+    public void transfer(double amount){
+        this.amount = amount;
+    }
+
+    public void request(double amount){
+        this.amount = amount;
+    }
+
+
+
     public static void main(String[] args) {
        
     }
