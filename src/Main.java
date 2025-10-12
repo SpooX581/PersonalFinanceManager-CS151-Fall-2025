6f927cd6869a1a03f0c2b8678bb3c5c518cefc69
Kennedy Nguyen <kennedynguyen2902@gmail.com>
2025-10-11 22:14:06 -0700

    clear() call

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.Scanner;
 
 public class Main {
@@ -74,6 +76,7 @@ public class Main {
         while(!end) {
             do {
                 // Clear the screen before operation
+                clear();
                 menu();
                 String input = scanner.nextLine();
                 exit(input);
