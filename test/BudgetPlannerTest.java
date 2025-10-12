1ea92526b7743329294cb2ef46e944048fb10065
lamchy3157 <chynalam@outlook.com>
2025-10-12 14:52:36 -0700

    add BudgetPlannerTest.java

--- test/BudgetPlannerTest.java
+++ test/BudgetPlannerTest.java
@@ -1,3 +1,19 @@
 public class BudgetPlannerTest {
+    public static void main(String[] args) {
+        //Below budget
+        BudgetPlanner belowBudget = new BudgetPlanner(500,400);
+        planner1.manageBudget();
+        System.out.println();
+
+        //Exact budget
+        BudgetPlanner exactBudget = new BudgetPlanner(800,800);
+        planner2.manageBudget();
+        System.out.println();
+
+        //Over budget
+        BudgetPlanner overBudget = new BudgetPlanner(800,1200);
+        planner3.manageBudget();
+    }
+
     
 }
