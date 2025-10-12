2ee62429c68c921375a98a297f9084ef97f24b2f
wlfrdoconc <wconcepbusiness@gmail.com>
2025-10-12 12:15:59 -0700

    created RecommendationEngine class, spendingAlerts, and cashbackSummary methods.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,65 +3,61 @@ public class BankAccount {
     private String accountNum;
     private String accountPin;
 
-    public BankAccount(double balance,String accountNum,String accountPin){
+    public BankAccount(double balance, String accountNum, String accountPin) {
         this.balance = balance;
         this.accountNum = accountNum;
         this.accountPin = accountPin;
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
-    public String getAccountNum(){
+
+    public String getAccountNum() {
         return accountNum;
     }
 
-    public String getAccountPin(){
+    public String getAccountPin() {
         return accountPin;
     }
 
-    public void withdraw(double amount){ 
-        if(amount <= balance){ 
-            balance = balance- amount;
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance = balance - amount;
             System.out.println("Transaction completed.");
-        }
-        else{
+        } else {
             System.out.println("Insufficient funds.");
         }
     }
 
-    public void deposit(double amount){
-        if(amount <= 0){
+    public void deposit(double amount) {
+        if (amount <= 0) {
             System.out.println("Amount deposited has to be positive.");
-        }
-        else{
+        } else {
             balance = balance + amount;
             System.out.println("Amount accepted.");
         }
     }
 
-    public void transfer(double amount){
-        if(amount > 0){
-            if(balance>= amount){
+    public void transfer(double amount) {
+        if (amount > 0) {
+            if (balance >= amount) {
                 balance = balance - amount;
                 System.out.println("Transfer complete.");
-            }
-            else{
+            } else {
                 System.out.println("Transfer declined. Insufficient funds.");
             }
-        }
-        else{
+        } else {
             System.out.println("Amount transferred has to be positive.");
         }
     }
 
-    public void request(double amount){
-        if(amount > 0){
+    public void request(double amount) {
+        if (amount > 0) {
             System.out.println("Request received.");
-        }
-        else{
+        } else {
             System.out.println("Amount requested has to be positive.");
-        }   
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package src;
-
 import java.util.Scanner;
 
 public class Main {
@@ -49,68 +47,71 @@ public class Main {
 
     }
 
-
     public static void main(String[] args) {
         User user = new User();
         register(user);
     }
 }
-        /*
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to our Personal Finance Manager! Let's get you started!");
-        System.out.println("Please enter your full name: ");
-        String fullName = scanner.nextLine();
-        System.out.println("Please create a username: ");
-        String username = scanner.nextLine();
-        System.out.println("Please create a password: ");
-        String password = scanner.nextLine();
-        System.out.println("Please enter your date of birth in MM/DD/YYYY format: ");
-        String dateOfBirth = scanner.nextLine();
-        System.out.println("Account creation successful " + fullName + "! Remember to keep your information safe.");
-        System.out.println("Please create an ID number.");
-        String idNumber = scanner.nextLine();
-
-        // finance manager system menu
-        System.out.println("Welcome back, " + fullName + "!");
-        System.out.println("Please type the option you would like to access: ");
-        System.out.println("1. User Information");
-        System.out.println("2. Bank Account");
-        System.out.println("3. Credit Card");
-        System.out.println("4. Finance Manager");
-        System.out.println("5. Loan Tracker");
-        System.out.println("6. Exit");
-
-        String response = scanner.nextLine();
-        if (response.equals("1") ||response.equals("User Information")) {
-
-        } else if (response.equals("2") || response.equals("Bank Account")) {
-            
-        } else if (response.equals("3") || response.equals("Credit Card")) {
-            CreditCard creditCard = new CreditCard();
-
-            System.out.println("What credit card company do you have?");
-            String cardCompany = scanner.nextLine();
-            creditCard.setCreditCardName(cardCompany);
-
-            System.out.println("What is your credit card number?");
-            String cardNumber = scanner.nextLine();
-            creditCard.setCreditCardNumber(cardNumber);
-
-            System.out.println("What is your statement balance?");
-            double currentBalance = scanner.nextDouble();
-            creditCard.setStatementBalance(currentBalance);
-
-        } else if (response.equals("4") || response.equals("Finance Manager")) {
-            
-        } else if (response.equals("5") || response.equals("Loan Tracker")) {
-            
-        } else if (response.equals("6") || response.equals("Exit") || response.equals("exit")) {
-            System.out.println("Thank you for coming today, have a good rest of your day!");
-            System.exit(0);
-        } else {
-            throw new IllegalArgumentException("Invalid user input. Please try again");
-        }
-        scanner.close();
-    }
-}
-         */
+/*
+ * Scanner scanner = new Scanner(System.in);
+ * System.out.
+ * println("Welcome to our Personal Finance Manager! Let's get you started!");
+ * System.out.println("Please enter your full name: ");
+ * String fullName = scanner.nextLine();
+ * System.out.println("Please create a username: ");
+ * String username = scanner.nextLine();
+ * System.out.println("Please create a password: ");
+ * String password = scanner.nextLine();
+ * System.out.println("Please enter your date of birth in MM/DD/YYYY format: ");
+ * String dateOfBirth = scanner.nextLine();
+ * System.out.println("Account creation successful " + fullName +
+ * "! Remember to keep your information safe.");
+ * System.out.println("Please create an ID number.");
+ * String idNumber = scanner.nextLine();
+ * 
+ * // finance manager system menu
+ * System.out.println("Welcome back, " + fullName + "!");
+ * System.out.println("Please type the option you would like to access: ");
+ * System.out.println("1. User Information");
+ * System.out.println("2. Bank Account");
+ * System.out.println("3. Credit Card");
+ * System.out.println("4. Finance Manager");
+ * System.out.println("5. Loan Tracker");
+ * System.out.println("6. Exit");
+ * 
+ * String response = scanner.nextLine();
+ * if (response.equals("1") ||response.equals("User Information")) {
+ * 
+ * } else if (response.equals("2") || response.equals("Bank Account")) {
+ * 
+ * } else if (response.equals("3") || response.equals("Credit Card")) {
+ * CreditCard creditCard = new CreditCard();
+ * 
+ * System.out.println("What credit card company do you have?");
+ * String cardCompany = scanner.nextLine();
+ * creditCard.setCreditCardName(cardCompany);
+ * 
+ * System.out.println("What is your credit card number?");
+ * String cardNumber = scanner.nextLine();
+ * creditCard.setCreditCardNumber(cardNumber);
+ * 
+ * System.out.println("What is your statement balance?");
+ * double currentBalance = scanner.nextDouble();
+ * creditCard.setStatementBalance(currentBalance);
+ * 
+ * } else if (response.equals("4") || response.equals("Finance Manager")) {
+ * 
+ * } else if (response.equals("5") || response.equals("Loan Tracker")) {
+ * 
+ * } else if (response.equals("6") || response.equals("Exit") ||
+ * response.equals("exit")) {
+ * System.out.
+ * println("Thank you for coming today, have a good rest of your day!");
+ * System.exit(0);
+ * } else {
+ * throw new IllegalArgumentException("Invalid user input. Please try again");
+ * }
+ * scanner.close();
+ * }
+ * }
+ */
