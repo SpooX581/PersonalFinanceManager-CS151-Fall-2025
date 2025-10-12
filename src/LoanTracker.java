dc2ae457026c3e9235e5f12a52f4288759acfd80
wlfrdoconc <wconcepbusiness@gmail.com>
2025-10-12 16:35:55 -0700

    created resource folder with bank account and credit card samples

--- src/LoanTracker.java
+++ src/LoanTracker.java
@@ -4,47 +4,47 @@ class LoanTracker {
     private double payment;
     private double totalPayments;
 
-    public LoanTracker (String loan, double amount, double totalPayments) {
+    public LoanTracker(String loan, double amount, double totalPayments) {
         this.loan = loan;
         this.amount = amount;
         this.payment = payment;
         this.totalPayments = totalPayments;
     }
 
-    public String getLoan(){
+    public String getLoan() {
         return loan;
     }
 
-    public double getAmount(){
+    public double getAmount() {
         return amount;
     }
 
-    public double payment(){
+    public double payment() {
         return payment;
     }
 
-    public double getTotalPayments(){
+    public double getTotalPayments() {
         return totalPayments;
     }
 
-    public void setLoan(){
+    public void setLoan() {
         this.loan = loan;
     }
 
-    public void setAmount(){
+    public void setAmount() {
         this.amount = amount;
     }
 
-    public void setPayment(){
+    public void setPayment() {
         this.payment = payment;
     }
 
-    public void setTotalPayments(){
+    public void setTotalPayments() {
         this.totalPayments = totalPayments;
     }
 
-    public void projection(double amount, double payment, double totalPayments){
-        totalPayments = amount/payment;
+    public void projection(double amount, double payment, double totalPayments) {
+        totalPayments = amount / payment;
         System.out.println("You have " + totalPayments + " payments remaining.");
     }
 
