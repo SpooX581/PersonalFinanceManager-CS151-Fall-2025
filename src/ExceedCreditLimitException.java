32102ca9d18695f64cc54df7a1dfab31f12b4be7
lamchy3157 <chynalam@outlook.com>
2025-10-12 22:49:01 -0700

    add ExceedCreditLimitException class

--- src/ExceedCreditLimitException.java
+++ src/ExceedCreditLimitException.java
@@ -2,4 +2,8 @@ public class ExceedCreditLimitException extends Exception {
     public ExceedCreditLimitException(String exceedCreditLimitMessage) {
         super(exceedCreditLimitMessage);
     }
+    @Override
+    public String toString() {
+        return "Credit Limit Error Message: " + getMessage();
+    }
 }
