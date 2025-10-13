27a8edaf09c8487c1396f4ba542fcafda1f69afb
lamchy3157 <chynalam@outlook.com>
2025-10-12 18:01:25 -0700

    Add BudgetPlanner.java 2nd commit

--- src/BudgetPlanner.java
+++ src/BudgetPlanner.java
@@ -12,17 +12,13 @@ public class BudgetPlanner {
         System.out.println("Your spending amount this month is $" +spending+".");
 
         if(spending<budget){
-            System.out.println("You have stayed within your budget this month.");
+            System.out.println("You have stayed within your budget this month. You have $"+(budget-spending)+" left over for yourself.");
         }
         else if(spending==budget){
-            System.out.println("You have reached your budget limit this month.");
+            System.out.println("You have reached your budget limit this month. You have $0 left over for yourself.");
         }
         else{
             System.out.println("You are over your budget limit this month by $"+(spending-budget)+".");
         }
-
     }
-    
-
-
 }
